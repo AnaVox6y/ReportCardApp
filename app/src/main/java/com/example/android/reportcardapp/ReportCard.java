fcb693156c67f98e85f7fb3a8f04da60767192d2
Annin7y <anast3222@gmail.com>
2020-11-19 10:15:41 -0500

    update code : gradle, libraries, license

--- app/src/main/java/com/example/android/reportcardapp/ReportCard.java
+++ app/src/main/java/com/example/android/reportcardapp/ReportCard.java
@@ -1,11 +1,22 @@
-package com.example.android.reportcardapp;
-
 /**
- * Created by Maino96-10022 on 10/19/2016.
+ *  Created by Anastasia Annin on 10/19/2016.
+ *  A Udacity Basics Nanodegree Project
+ *  Copyright 2020 Anastasia Annin
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  */
 
-public class ReportCard {
+package com.example.android.reportcardapp;
 
+public class ReportCard
+{
     private static final int MAX_CLASS_SIZE = 30;
     private int mMathMark;
     private int mScienceMark;
@@ -13,63 +24,62 @@ public class ReportCard {
     private int mHistoryMark;
     private int mPhysEdMark;
 
-
     public ReportCard(int MathMark, int ScienceMark, int EnglishMark,
-                      int HistoryMark, int PhysEdMark) {
-
+                      int HistoryMark, int PhysEdMark)
+    {
         mMathMark = MathMark;
         mScienceMark = ScienceMark;
         mEnglishMark = EnglishMark;
         mHistoryMark = HistoryMark;
         mPhysEdMark = PhysEdMark;
-
-
     }
 
-
-    public void setMathMark(int MathMark) {
+    public void setMathMark(int MathMark)
+    {
         mMathMark = MathMark;
     }
 
-    public int getMathMark() {
+    public int getMathMark()
+    {
         return mMathMark;
     }
 
-    public void setScienceMark(int ScienceMark) {
+    public void setScienceMark(int ScienceMark)
+    {
         mScienceMark = ScienceMark;
     }
 
     public int getScienceMark() {
         return mScienceMark;
-
     }
 
-    public void setEnglishMark(int EnglishMark) {
+    public void setEnglishMark(int EnglishMark)
+    {
         mEnglishMark = EnglishMark;
     }
 
-    public int getEnglishMark() {
+    public int getEnglishMark()
+    {
         return mEnglishMark;
-
     }
 
-    public void setHistoryMark(int HistoryMark) {
+    public void setHistoryMark(int HistoryMark)
+    {
         mHistoryMark = HistoryMark;
     }
 
-    public int getHistoryMark() {
+    public int getHistoryMark()
+    {
         return mHistoryMark;
-
     }
 
-    public void setPhysEdMark(int PhysEdMark) {
+    public void setPhysEdMark(int PhysEdMark)
+    {
         mPhysEdMark = PhysEdMark;
     }
 
-    public int getPhysEdMark() {
+    public int getPhysEdMark()
+    {
         return mPhysEdMark;
-
     }
-
-
 }
--- app/src/main/java/com/example/android/reportcardapp/ReportCardAdapter.java
+++ app/src/main/java/com/example/android/reportcardapp/ReportCardAdapter.java
@@ -1,3 +1,17 @@
+/**
+ *  Created by Anastasia Annin on 10/21/2016.
+ *  A Udacity Basics Nanodegree Project
+ *   Copyright 2020 Anastasia Annin
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ */
 package com.example.android.reportcardapp;
 
 import android.app.Activity;
@@ -9,44 +23,45 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by Maino96-10022 on 10/21/2016.
- */
-
-public class ReportCardAdapter extends ArrayAdapter<ReportCard> {
-
-
-    public ReportCardAdapter(Activity context, ArrayList<ReportCard> marks) {
+public class ReportCardAdapter extends ArrayAdapter<ReportCard>
+{
+    public ReportCardAdapter(Activity context, ArrayList<ReportCard> marks)
+    {
         super(context, 0, marks);
     }
 
-
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
         //first get the list item view you can use
         View listItemView = convertView;
-        if (listItemView == null) {
+        if (listItemView == null)
+        {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
 
-        ReportCard currentWord = getItem(position);
+        ReportCard reportCard = getItem(position);
 
         TextView mathTextView = (TextView) listItemView.findViewById(R.id.math_text_view);
-        mathTextView.setText(currentWord.getMathMark());
+        String stringValueMath = String.valueOf(reportCard.getMathMark());
+        mathTextView.setText(stringValueMath);
 
         TextView scienceTextView = (TextView) listItemView.findViewById(R.id.science_text_view);
-        scienceTextView.setText(currentWord.getScienceMark());
+        String stringValueScience = String.valueOf(reportCard.getScienceMark());
+        scienceTextView.setText(stringValueScience);
 
         TextView englishTextView = (TextView) listItemView.findViewById(R.id.english_text_view);
-        englishTextView.setText(currentWord.getEnglishMark());
+        String stringValueEnglish = String.valueOf(reportCard.getEnglishMark());
+        englishTextView.setText(stringValueEnglish);
 
         TextView historyTextView = (TextView) listItemView.findViewById(R.id.history_text_view);
-        historyTextView.setText(currentWord.getHistoryMark());
+        String stringValueHistory = String.valueOf(reportCard.getHistoryMark());
+        historyTextView.setText(stringValueHistory);
 
         TextView physedTextView = (TextView) listItemView.findViewById(R.id.physed_text_view);
-        physedTextView.setText(currentWord.getPhysEdMark());
-
+        String stringValuePhysEd = String.valueOf(reportCard.getPhysEdMark());
+        physedTextView.setText(stringValuePhysEd);
 
         return listItemView;
     }
